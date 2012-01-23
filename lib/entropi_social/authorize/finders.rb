5f77a761f9d9bdf96fd44e09d37430fa37c17412
Nicholas Watson <nwwatson@gmail.com>
2012-01-23 14:49:23 -0500

    updated viewable_for query. utilizing sqeel gem to generate or clause

--- lib/entropi_social/authorize/finders.rb
+++ lib/entropi_social/authorize/finders.rb
@@ -5,7 +5,7 @@ module EntropiSocial
         base.extend(ClassMethods)
         base.scope :by_profile_id, lambda { |profile_id| base.where(:profile_id => profile_id) }
         base.scope :by_membership_id, lambda { 
-          |profile_id| base.joins(:accesses => [:group => :memberships]).where(:memberships =>{:user_id => profile_id})
+          |id| base.includes(:accesses => [:group => :memberships]).where{(profile_id.eq id) | (memberships.user_id.eq id)}
         }
         base.scope :by_id, lambda { |base_id| base.where(:id => base_id).limit(1) }
       end
@@ -13,17 +13,17 @@ module EntropiSocial
       module ClassMethods
         # This model determins if it will send back entities owned by a user, or viewable by a user
         def for(requesting_user, for_profile)
-          requesting_user.id.eql?(for_profile.user_id) ? owned_by(for_profile) : viewable_for(for_profile)
+          requesting_user.id.eql?(for_profile.user_id) ? self.owned_by(for_profile) : self.viewable_for(for_profile)
         end
         
         # This mehtod gets all records that are owned by this particular profile
         def owned_by (profile)
-          by_profile_id(profile.id)
+          self.by_profile_id(profile.id)
         end
 
         # This method gets all the records that are viewable by a particular profile
         def viewable_for (profile)
-          by_membership_id(profile.id)
+          self.by_membership_id(profile.id)
         end
         
         def default_scoped?
