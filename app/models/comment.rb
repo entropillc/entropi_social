7af067ee9e045a7e81c9ffd13955f9a12a48cdef
Dan Sterrett <dansterrett@gmail.com>
2012-02-01 10:29:54 -0500

    Overrode the as_json method in the Comment model to include the profile of the comment author.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,11 @@ class Comment < ActiveRecord::Base
   
   # NOTE: Comments belong to a profile
   belongs_to :profile
+  
+  def as_json(options={})
+    hash = super.as_json
+    # add profile to json
+    hash["profile"] = self.profile
+    return hash    
+  end
 end
