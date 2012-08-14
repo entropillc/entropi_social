903301f156919cfdba56e1e7661abfb821af112a
Nicholas Watson <nwwatson@gmail.com>
2012-08-14 00:56:41 -0400

    move devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for  :users,
               :controllers => { :sessions => 'entropi_social/user_sessions',
                                 :registrations => 'entropi_social/user_registrations',
                                 :omniauth_callbacks => "entropi_social/omniauth_callbacks" },
-              :skip => [:unlocks],
               :path_names => { :sign_out => 'logout'}
-  
+
   devise_scope :user do
     get "/signin" => "entropi_social/user_sessions#new", :as => :login
     get "/signout" => "entropi_social/user_sessions#destroy", :as => :logout
     get "/signup" => "entropi_social/user_registrations#new", :as => :signup
     get "/profile" => "devise/registrations#edit"
-    #get '/users/auth/:provider' => 'omniauth_callbacks#passthru', :as => :passthru
   end
-  
+
 end
